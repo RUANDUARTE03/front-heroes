e7b6d0206b65310ac2ff1b61d0142b71220f9569
Ruan Duarte <ruanduarte0300@gmail.com>
2023-05-24 08:01:17 -0300

    add all settings at login

--- src/layouts/Login/index.tsx
+++ src/layouts/Login/index.tsx
@@ -1,8 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import * as S from "./styles";
 
 const Login = () => {
-  return <S.ContainerLogin></S.ContainerLogin>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  return (
+    <S.ContainerLogin>
+      <S.ContainerModalLogin>
+        <S.Title>Login Heroes</S.Title>
+        <S.BodyForm>
+          <S.InputInformation
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="E-mail"
+          />
+          <S.InputInformation
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder="Senha"
+          />
+        </S.BodyForm>
+        <S.BtnLogin>
+          <p>Login</p>
+        </S.BtnLogin>
+        <S.InformationRegister>
+          Ainda não tem cadastro? <span>Registre-se</span>
+        </S.InformationRegister>
+      </S.ContainerModalLogin>
+    </S.ContainerLogin>
+  );
 };
 
 export default Login;
--- src/layouts/Login/styles.ts
+++ src/layouts/Login/styles.ts
@@ -1,3 +1,76 @@
+import { light, primary, secondary } from "src/styles/variables";
 import styled from "styled-components";
 
-export const ContainerLogin = styled.div``;
+export const ContainerLogin = styled.div`
+  align-items: center;
+  display: flex;
+  height: calc(100vh - 100px - 42px);
+  justify-content: center;
+`;
+
+export const ContainerModalLogin = styled.div`
+  align-items: center;
+  background-color: ${primary};
+  border-radius: 8px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  padding: 24px 12px;
+  width: 400px;
+`;
+
+export const Title = styled.p`
+  color: ${light};
+  font-size: 32px;
+  font-weight: 400;
+`;
+
+export const BodyForm = styled.div`
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  margin: 8px 0 16px;
+  width: 100%;
+`;
+
+export const InputInformation = styled.input`
+  border-radius: 8px;
+  height: 40px;
+  margin-top: 12px;
+  padding-left: 8px;
+  width: 80%;
+
+  ::placeholder {
+    padding-left: 8px;
+  }
+`;
+
+export const BtnLogin = styled.div`
+  align-items: center;
+  background-color: ${light};
+  border-radius: 8px;
+  cursor: pointer;
+  display: flex;
+  height: 40px;
+  justify-content: center;
+  margin: 12px 0 20px;
+  width: 60%;
+
+  & p {
+    color: ${primary};
+    font-size: 16px;
+    font-weight: 700;
+  }
+`;
+
+export const InformationRegister = styled.p`
+  color: ${light};
+  font-size: 14px;
+  font-weight: 600;
+
+  & span {
+    color: ${secondary};
+    cursor: pointer;
+  }
+`;
